6ab24cbf35daa3017f366318ded9bf9ba0fde13d
Caroline Chen <yc4ag@virginia.edu>
2020-11-11 17:54:29 -0500

    send message ws (only one ws for now)

--- src/main/java/controllers/UiWebSocket.java
+++ src/main/java/controllers/UiWebSocket.java
@@ -5,10 +5,14 @@ import io.javalin.websocket.WsCloseHandler;
 import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsConnectHandler;
 import io.javalin.websocket.WsHandler;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Consumer;
 import org.eclipse.jetty.websocket.api.Session;
+
+import com.google.gson.Gson;
 
 /** Web socket class: DO NOT MODIFY.
  * @author Shirish Singh
@@ -18,6 +22,8 @@ public class UiWebSocket implements Consumer<WsHandler>  {
 
   // Store sessions to broadcast a message to all users
   private static final Queue<Session> SESSIONS = new ConcurrentLinkedQueue<>();
+  private static Map<WsConnectContext, Integer> userUsernameMap = new ConcurrentHashMap<>();
+  private static int nextUserNumber = 1;
 
   @Override
   public void accept(final WsHandler t) {
@@ -29,6 +35,9 @@ public class UiWebSocket implements Consumer<WsHandler>  {
       public void handleConnect(final WsConnectContext ctx) throws Exception {
         // TODO Auto-generated method stub
         SESSIONS.add(ctx.session);
+        int userid = nextUserNumber++; 
+        userUsernameMap.put(ctx, userid);
+        broadcastMessage("Server", (userid + " left the chat"));
       }
 
     });
@@ -38,6 +47,7 @@ public class UiWebSocket implements Consumer<WsHandler>  {
 
       @Override
       public void handleClose(final WsCloseContext ctx) throws Exception {
+    	ctx.send("someone leave");
         SESSIONS.remove(ctx.session);
       }
     });
@@ -45,6 +55,13 @@ public class UiWebSocket implements Consumer<WsHandler>  {
 
   public static Queue<Session> getSessions() {
     return SESSIONS;
+  }
+  
+  // Sends a message from one user to all users, along with a list of current usernames
+  private static void broadcastMessage(String sender, String message) {
+      userUsernameMap.keySet().stream().filter(ctx -> ctx.session.isOpen()).forEach(session -> {
+          session.send(new Gson().toJson(sender + ": " + message));
+      });
   }
 
 }
